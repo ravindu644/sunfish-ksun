8c106b8f3ea4e4d4db5b87b3f1961f94650ee7a6
Ravindu Deshan <Droidcasts@protonmail.com>
2025-04-17 19:24:29 +0000

    build script: install rest of the requirements
    Signed-off-by: Ravindu Deshan <Droidcasts@protonmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,8 +7,13 @@ source ${SCRIPT_DIR}/download_toolchain.sh
 if [ ! -f ".requirements" ]; then
     echo -e "\n[INFO]: INSTALLING REQUIREMENTS..!\n"
     {
-        sudo apt update
-        sudo apt install -y rsync p7zip-full
+
+    sudo apt update
+    sudo apt install -y rsync p7zip-full git device-tree-compiler lz4 xz-utils zlib1g-dev openjdk-17-jdk gcc g++ python3 python-is-python3 android-sdk-libsparse-utils erofs-utils \
+    default-jdk git gnupg flex bison gperf build-essential zip curl libc6-dev libncurses-dev libx11-dev libreadline-dev libgl1 libgl1-mesa-dev \
+    python3 make sudo gcc g++ bc grep tofrodos python3-markdown libxml2-utils xsltproc zlib1g-dev python-is-python3 libc6-dev libtinfo6 \
+    make repo cpio kmod openssl libelf-dev pahole libssl-dev libarchive-tools zstd --fix-missing
+
     } && touch .requirements
 fi
 
@@ -17,3 +22,5 @@ if [[ ! -d "${SCRIPT_DIR}/prebuilts" ]]; then
     download_ndk
     cd "${SCRIPT_DIR}"
 fi
+
+BUILD_CONFIG=private/msm-google/build.config.sunfish_no-cfi build/build.sh "$@"
